43c32ae84203c7e7061e83e97f54c00e85c0f595
gnetboy <gnetboy.y@gmail.com>
2021-08-05 11:58:17 -0700

    commit tree 3ca2c442452f2c3642a2beb3fac29307e358e82c

--- src/index.js
+++ src/index.js
@@ -13,3 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchToys(){
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(renderToys)
+}
+const toyCollection = document.getElementById('toy-collection')
+function renderToys(toys) {
+  toyCollection.innerHTML = ""
+  toys.forEach(function (toy) {
+    toyCollection.innerHTML += `
+   <div class="card" data-id=${toy.id}>
+        <h2>${toy.name}</h2>
+        <img src="${toy.image}" class="toy-avatar" />
+        <p>${toy.likes} Likes</p>
+        <button class="like-btn">Like <3</button>
+        <button class="delete-btn">Delete</button>
+   </div>
+  `
+  })
+}
